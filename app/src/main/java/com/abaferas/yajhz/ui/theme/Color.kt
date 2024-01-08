2a486742b1ae001d202f78786cd0140b1bea7c87
abaferas <wasfyelgamed@gmail.com>
2024-01-08 17:28:25 +0200

    add design colors

--- app/src/main/java/com/abaferas/yajhz/ui/theme/Color.kt
+++ app/src/main/java/com/abaferas/yajhz/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val color_beige = Color(0xFFF2CE92)
+val color_green = Color(0xFF0A977F)
+val color_black = Color(0xFF1C1C1C)
+val color_gray = Color(0xFF484848)
