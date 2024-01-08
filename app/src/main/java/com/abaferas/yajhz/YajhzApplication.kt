31ee6a2945215ef4cf49bfdaf9f35be3411649f2
abaferas <wasfyelgamed@gmail.com>
2024-01-08 17:51:05 +0200

    implement YajhzApplication and link MainActivity with navigation

--- app/src/main/java/com/abaferas/yajhz/YajhzApplication.kt
+++ app/src/main/java/com/abaferas/yajhz/YajhzApplication.kt
@@ -1,7 +1,9 @@
 package com.abaferas.yajhz
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class YajhzApplication: Application() {
 
 }
--- app/src/main/java/com/abaferas/yajhz/ui/MainActivity.kt
+++ app/src/main/java/com/abaferas/yajhz/ui/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.abaferas.yajhz.ui.navigation.YajhzNavigationApp
 import com.abaferas.yajhz.ui.theme.YajhzTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    YajhzNavigationApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YajhzTheme {
-        Greeting("Android")
     }
 }
